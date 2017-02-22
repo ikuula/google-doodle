10bfb49ddc72ec484e10702a57bb7914f9083d8e
Isaac Kuula <ikuula90@vcd-ch204-12.eastern.ewu.edu>
2017-02-22 13:45:07 -0800

    Randomized Image
    *Random Homepage images

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 // YOUR SCRIPTS GO HERE
+
+//Store 3 images
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img1 = '../img/400.jpg';
+
+//Get ramdom number
+//random number generator
+var max = 3;
+var randNum = Math.floor(Math.random() * max);
+console.log.(randNum);
+
+// Display randomized img
+$('img').attr('src', eval('img' + randNum) );
