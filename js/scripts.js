bcce44256907b1df3b0f78d171c27dc72fbd23e0
Isaac Kuula <ikuula90@gmail.com>
2017-02-22 18:09:28 -0800

    added textured "google"
    dialed in some random generated bg colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 // YOUR SCRIPTS GO HERE
 
-//Store 3 images
-var img0 = '../img/200.jpg';
-var img1 = '../img/300.jpg';
-var img1 = '../img/400.jpg';
+////Store 3 images
+//var img0 = '../img/200.jpg';
+//var img1 = '../img/300.jpg';
+//var img1 = '../img/400.jpg';
+//
+////Get ramdom number
+////random number generator
+//var max = 3;
+//var randNum = Math.floor(Math.random() * max);
+//console.log(randNum);
+//
+//// Display randomized img
+//$('#random').attr('src', eval('img' + randNum) );
 
-//Get ramdom number
-//random number generator
-var max = 3;
+
+
+//RANDOM BG GENERATOR
+//get random number 
+var max = 5;
 var randNum = Math.floor(Math.random() * max);
 console.log(randNum);
 
-// Display randomized img
-$('#random').attr('src', eval('img' + randNum) );
+//change class on body element
+$('body').addClass('bg' + randNum);
